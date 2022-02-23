3cee20d61d263e63e7969219b74ea79b88147d92
Vojtěch Hordějčuk <voho@users.noreply.github.com>
2022-02-23 22:11:04 +0100

    chore: setup projen for monocdk (#5)
    Co-authored-by: Vojta Hordějčuk 🦉 <vojta@amazon.com>

--- .projenrc.js
+++ .projenrc.js
@@ -2,14 +2,52 @@ const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'CDK Monitoring Constructs Team',
   authorAddress: 'monitoring-cdk-constructs@amazon.com',
-  cdkVersion: '2.1.0',
+  cdkVersion: '1.123.0',
   defaultReleaseBranch: 'main',
   name: 'cdk-monitoring-constructs',
   repositoryUrl: 'git@github.com:cdklabs/cdk-monitoring-constructs.git',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  cdkDependencies: ['monocdk'],
+  cdkTestDependencies: ['@monocdk-experiment/assert'],
+  eslint: true,
+  eslintOptions: {
+    env: {
+      es2021: true,
+    },
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint', 'unused-imports'],
+    rules: {
+      'unused-imports/no-unused-imports': 'error',
+      'no-case-declarations': 'off',
+      'no-bitwise': 'off',
+      'no-shadow': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-shadow': 'off',
+      '@typescript-eslint/member-ordering': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'prettier/prettier': [
+        'error',
+        {
+          arrowParens: 'always',
+          bracketSpacing: false,
+          printWidth: 140,
+          semi: true,
+          tabWidth: 4,
+          trailingComma: 'all',
+        },
+      ],
+    },
+  },
+  srcdir: 'lib',
+  testdir: 'test',
+  publishToPypi: {
+    distName: 'cdk-monitoring-constructs',
+    module: 'cdk_monitoring_constructs',
+  },
 });
 project.synth();
