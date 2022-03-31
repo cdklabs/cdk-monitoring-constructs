e005302112315c8f2d786a6b440d82028adbd90c
Eugene Cheung <81188333+echeung-amzn@users.noreply.github.com>
2022-03-31 10:22:43 -0400

    chore: configure releases from monocdk branch [skip ci] (#101)

--- .projenrc.js
+++ .projenrc.js
@@ -11,11 +11,11 @@ const project = new awscdk.AwsCdkConstructLibrary({
 
   defaultReleaseBranch: "main",
   majorVersion: 1,
-  // releaseBranches: {
-  //   monocdk: {
-  //     majorVersion: 0,
-  //   },
-  // },
+  releaseBranches: {
+    monocdk: {
+      majorVersion: 0,
+    },
+  },
   stability: "experimental",
 
   cdkVersion: CDK_VERSION,
