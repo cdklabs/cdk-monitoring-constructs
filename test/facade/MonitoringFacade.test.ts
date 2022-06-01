ce6754b6d548bb0eb2d7bdd811c8f2e710b690fe
Eugene Cheung <81188333+echeung-amzn@users.noreply.github.com>
2022-06-01 09:53:35 -0400

    fix(facade): avoid duplicated construct names for default dashboard factories (#154)
    If you have multiple facades in the same app using the default `dashboardFactory`, you'll end up with errors like:
    
    ```
    [...]/node_modules/constructs/lib/construct.js:335
                throw new Error(`There is already a Construct with name '${childName}' in ${typeName}${name.length > 0 ? ' [' + name + ']' : ''}`);
                ^
    
    Error: There is already a Construct with name 'Dashboards' in MyMonitoringFacade [MyApp-NestedTest]
    [...]
    ```
    
    ---
    
    _By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license_
    
    (cherry picked from commit 7ab639323322af73408fd13f9f2704ed8a54f4f5)

--- test/facade/MonitoringFacade.test.ts
+++ test/facade/MonitoringFacade.test.ts
@@ -3,15 +3,31 @@ import { Template } from "monocdk/assertions";
 import { MonitoringFacade } from "../../lib";
 
 describe("test of defaults", () => {
-  const stack = new Stack();
-  new MonitoringFacade(stack, "Test");
-  const result = Template.fromStack(stack);
-
   test("only default dashboard gets created by default", () => {
+    const stack = new Stack();
+    new MonitoringFacade(stack, "Test");
+    const result = Template.fromStack(stack);
+
     result.resourceCountIs("AWS::CloudWatch::Dashboard", 1);
 
     result.hasResourceProperties("AWS::CloudWatch::Dashboard", {
       DashboardName: "Test",
+    });
+  });
+
+  test("handles multiple facades", () => {
+    const stack = new Stack();
+    new MonitoringFacade(stack, "Test1");
+    new MonitoringFacade(stack, "Test2");
+    const result = Template.fromStack(stack);
+
+    result.resourceCountIs("AWS::CloudWatch::Dashboard", 2);
+
+    result.hasResourceProperties("AWS::CloudWatch::Dashboard", {
+      DashboardName: "Test1",
+    });
+    result.hasResourceProperties("AWS::CloudWatch::Dashboard", {
+      DashboardName: "Test2",
     });
   });
 });
