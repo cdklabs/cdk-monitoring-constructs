af65f338bed4acfc721341a936220989a8097e7b
Vojta Hordějčuk 🦉 <vojta@amazon.com>
2022-11-28 13:35:32 +0100

    feat(widget): header widget supports adding descriptiom

--- test/dashboard/widget/MonitoringHeaderWidget.test.ts
+++ test/dashboard/widget/MonitoringHeaderWidget.test.ts
@@ -36,3 +36,26 @@ test("snapshot test: with link and custom text", () => {
 
   expect(widget.toJson()).toMatchSnapshot();
 });
+
+test("snapshot test: with link, custom text and description", () => {
+  const widget = new MonitoringHeaderWidget({
+    title: "DummyTitle",
+    family: "DummyFamily",
+    goToLinkUrl: "http://amazon.com",
+    description: "CustomDescription",
+  });
+
+  expect(widget.toJson()).toMatchSnapshot();
+});
+
+test("snapshot test: with link, custom text and description with custom height", () => {
+  const widget = new MonitoringHeaderWidget({
+    title: "DummyTitle",
+    family: "DummyFamily",
+    goToLinkUrl: "http://amazon.com",
+    description: "CustomDescription",
+    descriptionHeight: 10,
+  });
+
+  expect(widget.toJson()).toMatchSnapshot();
+});
