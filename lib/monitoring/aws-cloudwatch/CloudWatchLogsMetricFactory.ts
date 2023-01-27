6414368c88793bb6611baa32639250b918455932
Eugene Cheung <81188333+echeung-amzn@users.noreply.github.com>
2023-01-26 23:15:13 -0500

    feat(cloudwatch): add addMinIncomingLogsAlarm (#310)
    Add ability to alarm on minimum number from `IncomingLogEvents` metric for a given CloudWatch log group. This is useful for detecting issues that would surface as missing incoming logs as a symptom (e.g., improper permissions, service outage/configuration issue, etc.).
    
    All available metrics: https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/CloudWatch-Logs-Monitoring-CloudWatch-Metrics.html
    
    ---
    
    _By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license_

---
+++ lib/monitoring/aws-cloudwatch/CloudWatchLogsMetricFactory.ts
@@ -0,0 +1,38 @@
+import { DimensionsMap } from "aws-cdk-lib/aws-cloudwatch";
+
+import { MetricFactory, MetricStatistic } from "../../common";
+
+const CloudWatchLogsNamespace = "AWS/Logs";
+
+export interface CloudWatchLogsMetricFactoryProps {
+  /**
+   * Name of the log group to monitor.
+   */
+  readonly logGroupName: string;
+}
+
+export class CloudWatchLogsMetricFactory {
+  private readonly metricFactory: MetricFactory;
+  private readonly dimensionsMap: DimensionsMap;
+
+  constructor(
+    metricFactory: MetricFactory,
+    props: CloudWatchLogsMetricFactoryProps
+  ) {
+    this.metricFactory = metricFactory;
+    this.dimensionsMap = {
+      LogGroupName: props.logGroupName,
+    };
+  }
+
+  metricIncomingLogEvents() {
+    return this.metricFactory.createMetric(
+      "IncomingLogEvents",
+      MetricStatistic.N,
+      "Logs",
+      this.dimensionsMap,
+      undefined,
+      CloudWatchLogsNamespace
+    );
+  }
+}
