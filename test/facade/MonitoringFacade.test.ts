39ed4ed8a8757dceef5a1dc8bad70014391105a3
David Dawson <djdawson@amazon.com>
2023-03-16 11:59:34 -0700

    adding unit test for dynamic dashboard factory

--- test/facade/MonitoringFacade.test.ts
+++ test/facade/MonitoringFacade.test.ts
@@ -1,6 +1,6 @@
 import { Stack } from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
-import { MonitoringFacade } from "../../lib";
+import { DefaultDynamicDashboardFactory, MonitoringFacade } from "../../lib";
 
 describe("test of defaults", () => {
   test("only default dashboard gets created by default", () => {
@@ -28,6 +28,33 @@ describe("test of defaults", () => {
     });
     result.hasResourceProperties("AWS::CloudWatch::Dashboard", {
       DashboardName: "Test2",
+    });
+  });
+
+  test("handles dynamic dashboards", () => {
+    const stack = new Stack();
+
+    const dynamicDashboardFactory = new DefaultDynamicDashboardFactory(
+      stack,
+      "TestDynamicDashboardFactory",
+      {
+        dashboardNamePrefix: "testPrefix",
+        dashboardConfigs: [{ name: "Dynamic1" }, { name: "Dynamic2" }],
+      }
+    );
+    new MonitoringFacade(stack, "Test1", {
+      dashboardFactory: dynamicDashboardFactory,
+    });
+    const result = Template.fromStack(stack);
+
+    result.resourceCountIs("AWS::CloudWatch::Dashboard", 2);
+
+    result.hasResourceProperties("AWS::CloudWatch::Dashboard", {
+      DashboardName: "testPrefix-Dynamic1",
+    });
+
+    result.hasResourceProperties("AWS::CloudWatch::Dashboard", {
+      DashboardName: "testPrefix-Dynamic2",
     });
   });
 });
