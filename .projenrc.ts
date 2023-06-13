b1fefddf4957dad9d99a243987b1b78601558e08
Momo Kornher <mail@moritzkornher.de>
2023-06-13 18:00:15 +0100

    chore: .projenrc.ts

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,10 +1,12 @@
-const { awscdk, javascript, DependencyType } = require("projen");
-const { workflows } = require("projen/lib/github");
+import { awscdk, javascript, DependencyType } from "projen";
+import { workflows } from "projen/lib/github";
+import { ArrowParens, TrailingComma } from "projen/lib/javascript";
 
 const CDK_VERSION = "2.65.0";
 
 const project = new awscdk.AwsCdkConstructLibrary({
   name: "cdk-monitoring-constructs",
+  projenrcTs: true,
   repositoryUrl: "https://github.com/cdklabs/cdk-monitoring-constructs",
   author: "CDK Monitoring Constructs Team",
   authorAddress: "monitoring-cdk-constructs@amazon.com",
@@ -67,12 +69,14 @@ _By submitting this pull request, I confirm that my contribution is made under t
   // Code linting config
   prettier: true,
   prettierOptions: {
-    arrowParens: "always",
-    bracketSpacing: false,
-    printWidth: 140,
-    semi: true,
-    tabWidth: 4,
-    trailingComma: "all",
+    settings: {
+      arrowParens: ArrowParens.ALWAYS,
+      bracketSpacing: false,
+      printWidth: 140,
+      semi: true,
+      tabWidth: 4,
+      trailingComma: TrailingComma.ALL,
+    },
   },
 });
 
@@ -95,7 +99,7 @@ _By submitting this pull request, I confirm that my contribution is made under t
 project.deps.addDependency("@types/prettier@2.6.0", DependencyType.DEVENV);
 
 // Add some other eslint rules followed across this project
-project.eslint.addRules({
+project.eslint?.addRules({
   "no-case-declarations": "off",
   "no-bitwise": "off",
   "no-shadow": "off",
@@ -112,10 +116,10 @@ project.eslint.addRules({
 project.addPackageIgnore("*.ts");
 project.addPackageIgnore("!*.d.ts");
 
-project.release.addJobs({
+project.release?.addJobs({
   notify_slack: {
     name: "Send Slack notification",
-    runsOn: "ubuntu-latest",
+    runsOn: ["ubuntu-latest"],
     permissions: {
       actions: workflows.JobPermission.READ,
     },
