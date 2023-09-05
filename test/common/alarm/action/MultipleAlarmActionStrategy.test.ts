333bbae1d626f0234dcd69f4f7929cffb844de05
Eugene Cheung <cheueuge@amazon.com>
2023-09-05 11:19:59 -0400

    feat(MultipleAlarmActionStrategy): add method to get flattened actions

--- test/common/alarm/action/MultipleAlarmActionStrategy.test.ts
+++ test/common/alarm/action/MultipleAlarmActionStrategy.test.ts
@@ -3,7 +3,11 @@ import { Template } from "aws-cdk-lib/assertions";
 import { Alarm, Metric } from "aws-cdk-lib/aws-cloudwatch";
 import { Topic } from "aws-cdk-lib/aws-sns";
 
-import { multipleActions, SnsAlarmActionStrategy } from "../../../../lib";
+import {
+  isMultipleAlarmActionStrategy,
+  multipleActions,
+  SnsAlarmActionStrategy,
+} from "../../../../lib";
 
 test("snapshot test: multiple actions", () => {
   const stack = new Stack();
@@ -15,12 +19,24 @@ test("snapshot test: multiple actions", () => {
     threshold: 0,
     metric: new Metric({ namespace: "Dummy", metricName: "Dummy" }),
   });
-  const action = multipleActions(
-    new SnsAlarmActionStrategy({ onAlarmTopic: topic1 }),
-    new SnsAlarmActionStrategy({ onAlarmTopic: topic2 }),
-    new SnsAlarmActionStrategy({ onAlarmTopic: topic3 })
-  );
+
+  const action1 = new SnsAlarmActionStrategy({ onAlarmTopic: topic1 });
+  const action2 = new SnsAlarmActionStrategy({ onAlarmTopic: topic2 });
+  const action3 = new SnsAlarmActionStrategy({ onAlarmTopic: topic3 });
+
+  const action = multipleActions(action1, action2, action3);
   action.addAlarmActions({ alarm, action });
 
   expect(Template.fromStack(stack)).toMatchSnapshot();
+  expect(action.flattenedAlarmActions()).toEqual([action1, action2, action3]);
+});
+
+test("isMultipleAlarmActionStrategy", () => {
+  const stack = new Stack();
+  const topic1 = new Topic(stack, "DummyTopic1");
+  const snsAction = new SnsAlarmActionStrategy({ onAlarmTopic: topic1 });
+  const multipleAction = multipleActions(snsAction);
+
+  expect(isMultipleAlarmActionStrategy(multipleAction)).toBe(true);
+  expect(isMultipleAlarmActionStrategy(snsAction)).toBe(false);
 });
