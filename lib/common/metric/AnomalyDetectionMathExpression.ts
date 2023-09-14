d3f77fabb83b8904aa6654818574b6d90f8d3b1a
Danny Zhao <zikangz@amazon.com>
2023-09-13 22:56:53 -0700

    Fix AnomalyDetectionMathExpression

--- lib/common/metric/AnomalyDetectionMathExpression.ts
+++ lib/common/metric/AnomalyDetectionMathExpression.ts
@@ -31,15 +31,29 @@ export class AnomalyDetectionMathExpression extends MathExpression {
   createAlarm(scope: Construct, id: string, props: CreateAlarmOptions): Alarm {
     const alarm = super.createAlarm(scope, id, props);
 
+    // `usingMetrics` of an anomaly detection alarm can only ever have one entry.
+    // Should the entry be a math expression, the math expression can have its own `usingMetrics`.
+    const finalExpressionId = Object.keys(this.usingMetrics)[0];
+
     // https://github.com/aws/aws-cdk/issues/10540#issuecomment-725222564
     const cfnAlarm = alarm.node.defaultChild as CfnAlarm;
     cfnAlarm.addPropertyDeletionOverride("Threshold");
     (cfnAlarm.metrics as CfnAlarm.MetricDataQueryProperty[]).forEach(
       (metric, index) => {
-        if (metric.expression) {
+        // To create an anomaly detection alarm, returned data should be set to true on two MetricDataQueryProperty(s):
+        // 1. The metric or math expression that is being evaluated for anomaly detection (eg. expr_1)
+        // 2. The actual expression of anomaly detection (eg. ANOMALY_DETECTION_BAND(expr_1, 1))
+        let returnData = false;
+
+        if (metric.expression?.includes("ANOMALY_DETECTION_BAND")) {
+          // thresholdMetricId is the ID of the ANOMALY_DETECTION_BAND function used as the threshold for the alarm.
           cfnAlarm.thresholdMetricId = metric.id;
+          returnData = true;
+        } else if (metric.id === finalExpressionId) {
+          returnData = true;
         }
-        cfnAlarm.addPropertyOverride(`Metrics.${index}.ReturnData`, true);
+
+        cfnAlarm.addPropertyOverride(`Metrics.${index}.ReturnData`, returnData);
       }
     );
 
