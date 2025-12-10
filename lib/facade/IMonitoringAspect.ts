42bd4988c0d807e7cca9070bb0c107365be14eeb
arkon <arkon@users.noreply.github.com>
2025-12-09 19:30:40 -0500

    feat: introduce replacement types for aspect

--- lib/facade/IMonitoringAspect.ts
+++ lib/facade/IMonitoringAspect.ts
@@ -30,52 +30,173 @@ import {
   WafV2MonitoringOptions,
 } from "../monitoring";
 
-export interface MonitoringAspectType<T> {
+/**
+ * @deprecated Extend {@link BaseMonitoringAspectType} instead.
+ *
+ * This type is not compatable with JSII@5.x.
+ */
+export type MonitoringAspectType<T> = BaseMonitoringAspectType & {
+  readonly props?: T;
+};
+
+export interface BaseMonitoringAspectType {
   /**
    * If the monitoring aspect is enabled for this resource.
    * @default - true
    */
   readonly enabled?: boolean;
+}
 
-  /**
-   * The monitoring props for this resource.
-   * @default - none
-   */
-  readonly props?: T;
+export interface CertificateManagerAspectType extends BaseMonitoringAspectType {
+  readonly props?: CertificateManagerMonitoringOptions;
+}
+
+export interface ApiGatewayAspectType extends BaseMonitoringAspectType {
+  readonly props?: ApiGatewayMonitoringOptions;
+}
+
+export interface ApiGatewayV2AspectType extends BaseMonitoringAspectType {
+  readonly props?: ApiGatewayV2MonitoringOptions;
+}
+
+export interface AppSyncAspectType extends BaseMonitoringAspectType {
+  readonly props?: AppSyncMonitoringOptions;
+}
+
+export interface AuroraClusterAspectType extends BaseMonitoringAspectType {
+  readonly props?: AuroraClusterMonitoringOptions;
+}
+
+export interface AutoScalingGroupAspectType extends BaseMonitoringAspectType {
+  readonly props?: AutoScalingGroupMonitoringOptions;
+}
+
+export interface BillingAspectType extends BaseMonitoringAspectType {
+  readonly props?: BillingMonitoringOptions;
+}
+
+export interface CloudFrontAspectType extends BaseMonitoringAspectType {
+  readonly props?: CloudFrontDistributionMonitoringOptions;
+}
+
+export interface CodeBuildAspectType extends BaseMonitoringAspectType {
+  readonly props?: CodeBuildProjectMonitoringOptions;
+}
+
+export interface DocumentDbAspectType extends BaseMonitoringAspectType {
+  readonly props?: DocumentDbMonitoringOptions;
+}
+
+export interface DynamoTableAspectType extends BaseMonitoringAspectType {
+  readonly props?: DynamoTableMonitoringOptions;
+}
+
+export interface EC2AspectType extends BaseMonitoringAspectType {
+  readonly props?: EC2MonitoringOptions;
+}
+
+export interface ElastiCacheAspectType extends BaseMonitoringAspectType {
+  readonly props?: ElastiCacheClusterMonitoringOptions;
+}
+
+export interface GlueJobAspectType extends BaseMonitoringAspectType {
+  readonly props?: GlueJobMonitoringOptions;
+}
+
+export interface KinesisDataAnalyticsAspectType
+  extends BaseMonitoringAspectType {
+  readonly props?: KinesisDataAnalyticsMonitoringOptions;
+}
+
+export interface KinesisDataStreamAspectType extends BaseMonitoringAspectType {
+  readonly props?: KinesisDataStreamMonitoringOptions;
+}
+
+export interface KinesisFirehoseAspectType extends BaseMonitoringAspectType {
+  readonly props?: KinesisFirehoseMonitoringOptions;
+}
+
+export interface LambdaFunctionAspectType extends BaseMonitoringAspectType {
+  readonly props?: LambdaFunctionMonitoringOptions;
+}
+
+export interface OpenSearchClusterAspectType extends BaseMonitoringAspectType {
+  readonly props?: OpenSearchClusterMonitoringOptions;
+}
+
+export interface RdsClusterAspectType extends BaseMonitoringAspectType {
+  readonly props?: RdsClusterMonitoringOptions;
+}
+
+export interface RdsInstanceAspectType extends BaseMonitoringAspectType {
+  readonly props?: RdsInstanceMonitoringOptions;
+}
+
+export interface RedshiftClusterAspectType extends BaseMonitoringAspectType {
+  readonly props?: RedshiftClusterMonitoringOptions;
+}
+
+export interface S3BucketAspectType extends BaseMonitoringAspectType {
+  readonly props?: S3BucketMonitoringOptions;
+}
+
+export interface SecretsManagerSecretAspectType
+  extends BaseMonitoringAspectType {
+  readonly props?: SecretsManagerSecretMonitoringOptions;
+}
+
+export interface SnsTopicAspectType extends BaseMonitoringAspectType {
+  readonly props?: SnsTopicMonitoringOptions;
+}
+
+export interface SqsQueueAspectType extends BaseMonitoringAspectType {
+  readonly props?: SqsQueueMonitoringOptions;
+}
+
+export interface StepFunctionAspectType extends BaseMonitoringAspectType {
+  readonly props?: StepFunctionMonitoringOptions;
+}
+
+export interface SyntheticsCanaryAspectType extends BaseMonitoringAspectType {
+  readonly props?: SyntheticsCanaryMonitoringOptions;
+}
+
+export interface WafV2AspectType extends BaseMonitoringAspectType {
+  readonly props?: WafV2MonitoringOptions;
 }
 
 export interface MonitoringAspectProps {
-  readonly acm?: MonitoringAspectType<CertificateManagerMonitoringOptions>;
-  readonly apiGateway?: MonitoringAspectType<ApiGatewayMonitoringOptions>;
-  readonly apiGatewayV2?: MonitoringAspectType<ApiGatewayV2MonitoringOptions>;
-  readonly appSync?: MonitoringAspectType<AppSyncMonitoringOptions>;
-  readonly auroraCluster?: MonitoringAspectType<AuroraClusterMonitoringOptions>;
-  readonly autoScalingGroup?: MonitoringAspectType<AutoScalingGroupMonitoringOptions>;
-  readonly billing?: MonitoringAspectType<BillingMonitoringOptions>;
-  readonly cloudFront?: MonitoringAspectType<CloudFrontDistributionMonitoringOptions>;
-  readonly codeBuild?: MonitoringAspectType<CodeBuildProjectMonitoringOptions>;
-  readonly documentDb?: MonitoringAspectType<DocumentDbMonitoringOptions>;
-  readonly dynamoDB?: MonitoringAspectType<DynamoTableMonitoringOptions>;
-  readonly ec2?: MonitoringAspectType<EC2MonitoringOptions>;
-  readonly elasticCache?: MonitoringAspectType<ElastiCacheClusterMonitoringOptions>;
-  readonly glue?: MonitoringAspectType<GlueJobMonitoringOptions>;
-  readonly kinesisDataAnalytics?: MonitoringAspectType<KinesisDataAnalyticsMonitoringOptions>;
-  readonly kinesisDataStream?: MonitoringAspectType<KinesisDataStreamMonitoringOptions>;
-  readonly kinesisFirehose?: MonitoringAspectType<KinesisFirehoseMonitoringOptions>;
-  readonly lambda?: MonitoringAspectType<LambdaFunctionMonitoringOptions>;
-  readonly openSearch?: MonitoringAspectType<OpenSearchClusterMonitoringOptions>;
+  readonly acm?: CertificateManagerAspectType;
+  readonly apiGateway?: ApiGatewayAspectType;
+  readonly apiGatewayV2?: ApiGatewayV2AspectType;
+  readonly appSync?: AppSyncAspectType;
+  readonly auroraCluster?: AuroraClusterAspectType;
+  readonly autoScalingGroup?: AutoScalingGroupAspectType;
+  readonly billing?: BillingAspectType;
+  readonly cloudFront?: CloudFrontAspectType;
+  readonly codeBuild?: CodeBuildAspectType;
+  readonly documentDb?: DocumentDbAspectType;
+  readonly dynamoDB?: DynamoTableAspectType;
+  readonly ec2?: EC2AspectType;
+  readonly elasticCache?: ElastiCacheAspectType;
+  readonly glue?: GlueJobAspectType;
+  readonly kinesisDataAnalytics?: KinesisDataAnalyticsAspectType;
+  readonly kinesisDataStream?: KinesisDataStreamAspectType;
+  readonly kinesisFirehose?: KinesisFirehoseAspectType;
+  readonly lambda?: LambdaFunctionAspectType;
+  readonly openSearch?: OpenSearchClusterAspectType;
   /**
-   * @deprecated Use rdsCluster instead.
+   * @deprecated Use {@link rdsCluster} instead.
    */
-  readonly rds?: MonitoringAspectType<RdsClusterMonitoringOptions>;
-  readonly rdsCluster?: MonitoringAspectType<RdsClusterMonitoringOptions>;
-  readonly rdsInstance?: MonitoringAspectType<RdsInstanceMonitoringOptions>;
-  readonly redshift?: MonitoringAspectType<RedshiftClusterMonitoringOptions>;
-  readonly s3?: MonitoringAspectType<S3BucketMonitoringOptions>;
-  readonly secretsManager?: MonitoringAspectType<SecretsManagerSecretMonitoringOptions>;
-  readonly sns?: MonitoringAspectType<SnsTopicMonitoringOptions>;
-  readonly sqs?: MonitoringAspectType<SqsQueueMonitoringOptions>;
-  readonly stepFunctions?: MonitoringAspectType<StepFunctionMonitoringOptions>;
-  readonly syntheticsCanaries?: MonitoringAspectType<SyntheticsCanaryMonitoringOptions>;
-  readonly webApplicationFirewallAclV2?: MonitoringAspectType<WafV2MonitoringOptions>;
+  readonly rds?: RdsClusterAspectType;
+  readonly rdsCluster?: RdsClusterAspectType;
+  readonly rdsInstance?: RdsInstanceAspectType;
+  readonly redshift?: RedshiftClusterAspectType;
+  readonly s3?: S3BucketAspectType;
+  readonly secretsManager?: SecretsManagerSecretAspectType;
+  readonly sns?: SnsTopicAspectType;
+  readonly sqs?: SqsQueueAspectType;
+  readonly stepFunctions?: StepFunctionAspectType;
+  readonly syntheticsCanaries?: SyntheticsCanaryAspectType;
+  readonly webApplicationFirewallAclV2?: WafV2AspectType;
 }
