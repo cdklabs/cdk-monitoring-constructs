b69e97aa80707d6e84fbe1807fed4a82e9bd69e1
arkon <4098258+arkon@users.noreply.github.com>
2025-12-10 23:14:36 -0500

    feat: merge KeyValueTableWidget and KeyValueTableWidgetV2 (#688)
    This is probably the smarter way of changing the constructor param types
    rather than just having an entirely new class.
    
    ---
    
    Example change to replace deprecated constructor usage:
    
    ```diff
    new KeyValueTableWidget([
    -  ["foo", "bar"],
    +  { key: "foo", value: "bar" },
    -  ["baz", "qux"],
    +  { key: "baz", value: "qux" },
    ]);
    ```
    
    ---
    
    Example change to replace deprecated V2 usage:
    
    ```diff
    - new KeyValueTableWidgetV2([
    + new KeyValueTableWidget([
      { key: "foo", value: "bar" },
      { key: "baz", value: "qux" },
    ]);
    ```
    
    ---
    
    _By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache-2.0 license_
    
    Co-authored-by: arkon <arkon@users.noreply.github.com>

--- lib/dashboard/widget/KeyValueTableWidget.ts
+++ lib/dashboard/widget/KeyValueTableWidget.ts
@@ -1,12 +1,68 @@
-import { KeyValueTableWidgetV2 } from "./KeyValueTableWidgetV2";
+import { TextWidget } from "aws-cdk-lib/aws-cloudwatch";
+
+import { FullWidth } from "../../common/widget";
+
+export interface KeyValue {
+  readonly key: string;
+  readonly value: string;
+}
 
 /**
- * @deprecated Use {@link KeyValueTableWidgetV2} instead.
- *
- * This class is not compatable with JSII@5.x.
+ * A widget that displays key-value pairs in a table format.
  */
-export class KeyValueTableWidget extends KeyValueTableWidgetV2 {
-  constructor(data: [string, string][]) {
-    super(data.map(([key, value]) => ({ key, value })));
+export class KeyValueTableWidget extends TextWidget {
+  /**
+   * Creates a key-value table widget.
+   * @param data An array of key-value pairs as objects
+   */
+  constructor(data: KeyValue[]);
+
+  /**
+   * Creates a key-value table widget.
+   * @param data An array of key-value pairs as tuples
+   *
+   * @deprecated Use the KeyValue[] constructor instead. This tuple format is not compatible with JSII 5.x and will be removed in the next major version.
+   */
+  constructor(data: [string, string][]);
+
+  constructor(data: KeyValue[] | [string, string][]) {
+    const normalizedData = KeyValueTableWidget.normalizeData(data);
+    super({
+      width: FullWidth,
+      height: 3,
+      markdown: KeyValueTableWidget.toMarkdown(normalizedData),
+    });
+  }
+
+  private static normalizeData(
+    data: KeyValue[] | [string, string][],
+  ): KeyValue[] {
+    if (data.length === 0) {
+      return [];
+    }
+
+    // Check if it's a tuple array by inspecting the first element
+    if (Array.isArray(data[0])) {
+      return (data as [string, string][]).map(([key, value]) => ({
+        key,
+        value,
+      }));
+    }
+
+    return data as KeyValue[];
+  }
+
+  private static toMarkdown(data: KeyValue[]) {
+    let headerRow = "";
+    let subHeaderRow = "";
+    let valueRow = "";
+
+    data.forEach(({ key, value }) => {
+      headerRow += "| " + key;
+      subHeaderRow += "|---";
+      valueRow += "| " + value;
+    });
+
+    return `${headerRow}\n${subHeaderRow}\n${valueRow}`;
   }
 }
--- test/dashboard/widget/KeyValueTableWidget.test.ts
+++ test/dashboard/widget/KeyValueTableWidget.test.ts
@@ -1,11 +1,29 @@
-import { KeyValueTableWidget } from "../../../lib/dashboard/widget";
+import {
+  KeyValueTableWidget,
+  KeyValueTableWidgetV2,
+  KeyValue,
+} from "../../../lib/dashboard/widget";
 
 test("snapshot test", () => {
+  const data: KeyValue[] = [
+    { key: "name", value: "John Wick" },
+    { key: "has", value: "a dog" },
+  ];
+  const widget = new KeyValueTableWidget(data);
+  const widgetV2 = new KeyValueTableWidgetV2(data);
+
+  expect(widget.toJson()).toMatchSnapshot();
+  expect(widget.toJson()).toEqual(widgetV2.toJson());
+});
+
+test("snapshot test with deprecated constructor", () => {
   const data: [string, string][] = [
     ["name", "John Wick"],
     ["has", "a dog"],
   ];
   const widget = new KeyValueTableWidget(data);
+  const widgetV2 = new KeyValueTableWidgetV2(data);
 
   expect(widget.toJson()).toMatchSnapshot();
+  expect(widget.toJson()).toEqual(widgetV2.toJson());
 });
